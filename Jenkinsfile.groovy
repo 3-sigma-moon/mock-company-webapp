ae0f3933874242385b1966be37a1db16b6b1def8
SIGMA <82468352+3-sigma-moon@users.noreply.github.com>
2024-04-29 19:06:45 +0000

    initial jenkins pipeline starter

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,14 @@
 pipeline {
     agent any
+    withEnv(['YARN_HOME=C:/Users/maiga/AppData/Roaming/npm/node_modules/yarn/bin']) {
+        stage('Prepare') {
+            steps {
+                bat "npm install -g yarn"
+                bat "yarn install"
+            }
 
+        }
+    }
     stages {
         stage('Prepare') {
             steps {
@@ -9,15 +17,7 @@ pipeline {
             }
 
         }
-        withEnv(['YARN_HOME=C:/Users/maiga/AppData/Roaming/npm/node_modules/yarn/bin']) {
-            stage('Prepare') {
-                steps {
-                    bat "npm install -g yarn"
-                    bat "yarn install"
-                }
 
-            }
-        }
         stage("Build") {
             steps {
                 bat " cd C:/Users/maiga/Downloads/accenture_advanced_software_engineering/mock-company-webapp"
