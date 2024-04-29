dddc06900f9edfdf65718b4ce4b46e37e627412f
SIGMA <82468352+3-sigma-moon@users.noreply.github.com>
2024-04-29 18:55:21 +0000

    initial jenkins pipeline starter

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,14 @@
 pipeline {
     agent any
-//    node {
-//        withEnv(['YARN_HOME=C:/Users/maiga/AppData/Roaming/npm/node_modules/yarn']) {
-//            bat '$YARN_HOME/bin'
-//        }
-//    }
+    node {
+
+    }
     stages {
 
         stage("Build") {
+            withEnv(['YARN_HOME=C:/Users/maiga/AppData/Roaming/npm/node_modules/yarn']) {
+                bat '$YARN_HOME/bin'
+            }
             steps {
                 bat " cd C:/Users/maiga/Downloads/accenture_advanced_software_engineering/mock-company-webapp"
                 echo "Building Stage Start"
@@ -16,6 +17,9 @@ pipeline {
             }
         }
         stage("Test") {
+            withEnv(['YARN_HOME=C:/Users/maiga/AppData/Roaming/npm/node_modules/yarn']) {
+                bat '$YARN_HOME/bin'
+            }
             steps {
                 echo "Testing Stage Start"
                 bat "./gradlew test"
