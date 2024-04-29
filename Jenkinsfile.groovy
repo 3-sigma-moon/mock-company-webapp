6dd1ab765199930697c9fcdfc94f312a50274b5d
SIGMA <82468352+3-sigma-moon@users.noreply.github.com>
2024-04-29 17:15:55 +0000

    Adding initial jenkins pipeline

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,19 @@
+pipeline{
+    agent any
+    stages{
+        stage("Build"){
+            steps{
+                echo "Building Stage Start"
+                bat "./gradlew assemble"
+                echo "Building Stage End"
+            }
+        }
+        stage("Test"){
+            steps{
+                echo "Testing Stage Start"
+                bat "./gradlew test"
+                echo "Testing Stage End"
+            }
+        }
+    }
+}
